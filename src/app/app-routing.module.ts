0b1239995800c7452c535b08b49e459ce184aeae
sirin912 <cyrineouni123@gmail.com>
2023-03-23 09:50:25 +0100

    atelier3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { NnavComponent } from './nnav/nnav.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'login',pathMatch:'full'},
+  {path:'login',component:LoginComponent},
+  {path:'addproduct',component:AddProductComponent},
+  {path:'emploi',component:OffresEmploiComponentComponent},
+  {path:'articles',component:ArticlesComponent},
+  {path:'**',component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { OffresEmploiComponentComponent } from './offres-emploi-component/offres-emploi-component.component';
 import { ArticlesComponent } from './articles/articles.component';
+import { LoginComponent } from './login/login.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { HeaderComponent } from './header/header.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AddproduitComponent } from './Core/ProductsManager/addproduit/addproduit.component';
+import { UpdateproduitComponent } from './Core/ProductsManager/updateproduit/updateproduit.component';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +24,14 @@ import { ArticlesComponent } from './articles/articles.component';
     NnavComponent,
     FooterComponent,
     OffresEmploiComponentComponent,
-    ArticlesComponent
+    ArticlesComponent,
+    LoginComponent,
+    AddProductComponent,
+    HeaderComponent,
+    NotFoundComponent,
+    AddproduitComponent,
+    UpdateproduitComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
